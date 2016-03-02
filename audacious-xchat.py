8dea1f3064f29375ce2b5326d51ccd98574d79a3
Jose Maldonado <yukiteruamano@openmailbox.org>
2016-03-01 23:06:49 -0430

    Modificaciones a los scripts de Audacious para Hexchat y Xchat

--- audacious-xchat.py
+++ audacious-xchat.py
@@ -1,22 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 __module_name__ = "Audacious2 MP3"
 __module_version__ = "1.0"
 __module_description__ = "Otorga informacion sobre la reproduccion de musica en Audacious"
 
-from dbus import Bus, DBusException
 import xchat
 import commands
-bus = Bus(Bus.TYPE_SESSION)
-
-def check_audacious():
-    try:
-        return bus.get_object('org.mpris.audacious', '/Player')
-    except DBusException:
-        print "\x02Audacious no esta ejecutandose, o ha ocurrido un error en el subsistema DBUS"
-        return None
 
 def mp3_info(word, word_eol, userdata):
 
-    check_audacious()
     artist = commands.getoutput("audtool --current-song-tuple-data artist")
     titulo = commands.getoutput("audtool --current-song-tuple-data title")
     album = commands.getoutput("audtool --current-song-tuple-data album")
